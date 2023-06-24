2c0d720c97448316b561da68bb921e8778b11593
Facundo <facuugomez67@gmail.com>
2023-06-24 18:50:40 -0300

    fixed-text

--- FRONTEND/src/components/Footer/Footer.jsx
+++ FRONTEND/src/components/Footer/Footer.jsx
@@ -117,6 +117,7 @@ export default function footer() {
 						<div className='cover_ubi'>
 							<div className='ubi'>
 								<p>Arieta 2809, San Justo, Buenos Aires</p>
+								<p>4441-8678</p>
 
 								<iframe
 									src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3281.1538133813438!2d-58.56659472549342!3d-34.67606726132062!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcc62e42276df5%3A0x4190efc310e17020!2sDr.%20Ignacio%20Arieta%202809%2C%20B1754APW%20San%20Justo%2C%20Provincia%20de%20Buenos%20Aires!5e0!3m2!1ses!2sar!4v1685508697776!5m2!1ses!2sar'
@@ -126,6 +127,19 @@ export default function footer() {
 
 							<div className='ubi'>
 								<p>Calle 26 Nº545, Gral. Pico, La Pampa</p>
+								<p>02302-323641</p>
+
+
+								<iframe
+									src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3241.8436114703663!2d-63.75478882543033!3d-35.65622451384127!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95c37d592b74ec9d%3A0x46cf814609ecbfc3!2sC.%2026%20545%2C%20Gral.%20Pico%2C%20La%20Pampa!5e0!3m2!1ses!2sar!4v1685559563647!5m2!1ses!2sar'
+									loading='lazy'
+								></iframe>
+							</div>
+
+							<div className='ubi'>
+								<p>Bv. 25 de Mayo N°101, Quemú-Quemú, La Pampa</p>
+								<p>2302-454512</p>
+
 
 								<iframe
 									src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3241.8436114703663!2d-63.75478882543033!3d-35.65622451384127!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95c37d592b74ec9d%3A0x46cf814609ecbfc3!2sC.%2026%20545%2C%20Gral.%20Pico%2C%20La%20Pampa!5e0!3m2!1ses!2sar!4v1685559563647!5m2!1ses!2sar'
